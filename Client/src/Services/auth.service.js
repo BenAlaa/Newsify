fd44b014a571179ce16d9c748b69304ea28216c0
BenAlaa <46384841+BenAlaa@users.noreply.github.com>
2020-04-03 22:07:53 +0200

    add auth service

---
+++ Client/src/Services/auth.service.js
@@ -0,0 +1,54 @@
+import jwt from 'jsonwebtoken';
+import jwtDecode from 'jwt-decode';
+import http from './http.service';
+import { apiUrl, tokenKey} from '../config.json';
+
+const apiEndpoint = apiUrl + "/auth";
+
+async function login(email, password) {
+    const {data: response} = await http.post(apiEndpoint, {email, password});
+    if (response.status !== 200) return {status: response.status, message: response.message};
+    const {data: jwt} = response;
+    if (!jwt) return {status: 400, message: "Faild to generate Token..."};
+    localStorage.setItem(tokenKey, jwt);
+    return {status: 200, message: "User Logged Successfully..."};
+}
+
+function loginWithJwt(jwt) {
+    localStorage.setItem(tokenKey, jwt);
+}
+
+function logout() {
+    localStorage.removeItem(tokenKey);
+}
+
+function generateAuthToken(user) {
+    const token = jwt.sign({ 
+        id: user.id, 
+        name: user.name, 
+        email: user.email
+    }, "jwtPrivateKey");
+    return token;
+}
+
+function getCurrentUser() {
+    try {
+        const jwt =JSON.stringify(localStorage.getItem(tokenKey));
+        return jwtDecode(jwt);
+    } catch (error) {
+        return null;
+    }
+}
+
+function getJwt(tokenKey) {
+    return localStorage.getItem(tokenKey);
+}
+
+export{
+    login,
+    loginWithJwt,
+    logout,
+    generateAuthToken,
+    getCurrentUser,
+    getJwt
+}
---
+++ Client/src/Services/http.service.js
@@ -0,0 +1,29 @@
+import axios from "axios";
+import { Promise } from "q";
+import {toast} from 'react-toastify';
+import {getJwt} from './auth.service';
+import { tokenKey} from '../config.json';
+
+
+axios.interceptors.response.use(null, error => {
+    const expectedError = error.response && error.response.status >= 400 && error.response.status < 500;
+    if(!expectedError){
+        console.log("Logging the error ", error);
+        toast.error("An unexpected error occurred.");
+    } 
+
+    return Promise.reject(error);
+});
+
+export function setJwt(jwt) {
+    axios.defaults.headers.common[tokenKey] = jwt;
+}
+axios.defaults.headers.common[tokenKey] = getJwt();
+
+export default {
+    get: axios.get,
+    post: axios.post,
+    put: axios.put,
+    delete: axios.delete,
+    setJwt
+}
