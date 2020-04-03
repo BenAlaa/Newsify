a9a17ce13abb448d73a14d9eb3403877942a9e14
BenAlaa <46384841+BenAlaa@users.noreply.github.com>
2020-04-03 22:10:18 +0200

    set app router

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,25 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Route, Redirect, Switch } from "react-router-dom";
+import ProtectedRoute from './Components/Common/ProtectedRoute/protectedRoute.componenet';
+import HomePage from './Pages/HomePage/home.page';
+import LoginPage from './Pages/LoginPage/login.page';
+import Logout from './Components/Logout/logout.component';
+import SourcesPage from './Pages/SourcesPage/sources.page';
+import NotFoundPage from './Pages/NotFoundPage/notFound.page';
+import {getCurrentUser} from './Services/auth.service';
 import './App.css';
 
-function App() {
+function App(props) {
+  const isLoggedin = getCurrentUser() ? true : false;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <Switch>
+        <Route path="/login"  render={(props) => <LoginPage {...props} history={props.history} isLoggedin={isLoggedin}/>}></Route>
+        <Route path="/logout" component={Logout} />
+        <Route path="/home" component={ProtectedRoute(HomePage, isLoggedin)} />
+        <Route path="/sources" component={ProtectedRoute(SourcesPage, isLoggedin)} />
+        <Route path="/not-found" component={ProtectedRoute(NotFoundPage, isLoggedin)} />
+        <Redirect from="/" exact to="/home" />
+        <Redirect to="/not-found" />
+      </Switch>
+    </React.Fragment>
   );
 }
 
