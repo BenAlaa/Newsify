9821f33cede29e8f00d105536f04665dc7b5ef98
BenAlaa <46384841+BenAlaa@users.noreply.github.com>
2020-04-03 22:08:51 +0200

    create home page

---
+++ Client/src/Pages/HomePage/home.page.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './home.styles.css';
+class HomePage extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <h1>Home Page</h1>
+         );
+    }
+}
+ 
+export default HomePage;
