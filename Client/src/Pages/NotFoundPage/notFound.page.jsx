0cad47d44b1eef04562fa79ca750710f62b4e62a
BenAlaa <46384841+BenAlaa@users.noreply.github.com>
2020-04-03 23:41:04 +0200

    complete and style not found page

--- Client/src/Pages/NotFoundPage/notFound.page.jsx
+++ Client/src/Pages/NotFoundPage/notFound.page.jsx
@@ -1,11 +1,24 @@
 import * as React from 'react';
 import {Link} from 'react-router-dom';
+import {Container, Row, Col} from 'styled-bootstrap-grid';
+import logo from './Assets/404.png'
 import "./notFound.styles.css";
 
 const NotFound = () => {
     return (
         <div className="notfound-container">
-            <h1>Not Found</h1>
+            <Container>
+                <Row>
+                    <Col xs={12} sm={10} md={8} lg={8}  xl={8} xsOffset={0} smOffset={1} mdOffset={2} lgOffset={2} xlOffset={2}>
+                        <img src={logo}></img>
+                        <h1>Oops! Page Not Be Found</h1>
+                        <p>Sorry but the page you are looking for does not exist, have been removed, name changed or is temporarily unavailable</p>
+                        <div className="link-container">
+                            <Link to="/home" >Back to home page</Link>
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
         </div>
     )
 }
--- Client/src/index.js
+++ Client/src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter} from 'react-router-dom'
 import App from './App';
-
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap";
 import './index.css';
 
 ReactDOM.render(
