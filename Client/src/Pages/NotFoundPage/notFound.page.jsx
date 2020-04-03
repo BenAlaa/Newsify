4cc95bf7b55dc95dd825363755810b27be2fde47
BenAlaa <46384841+BenAlaa@users.noreply.github.com>
2020-04-03 22:09:31 +0200

    create not found

---
+++ Client/src/Pages/NotFoundPage/notFound.page.jsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import {Link} from 'react-router-dom';
+import "./notFound.styles.css";
+
+const NotFound = () => {
+    return (
+        <div className="notfound-container">
+            <h1>Not Found</h1>
+        </div>
+    )
+}
+
+export default NotFound;
