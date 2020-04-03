b2792dcedada42f09a2928197fda1c03f647beda
BenAlaa <46384841+BenAlaa@users.noreply.github.com>
2020-04-03 22:09:59 +0200

    create sources page

---
+++ Client/src/Pages/SourcesPage/sources.page.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import './sources.styles.css';
+
+class SourcesPage extends Component {
+    state = {  }
+    render() { 
+        return ( <h1>Sources Page</h1> );
+    }
+}
+ 
+export default SourcesPage;
