bee05202a18df7847554dc08ddc20590859ca76b
BenAlaa <46384841+BenAlaa@users.noreply.github.com>
2020-04-05 01:55:29 +0200

    add news card component

---
+++ Client/src/Components/NewsCard/newsCard.component.jsx
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import PropTypes from "prop-types";
+import { faLink, faNewspaper } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {  dateFormater } from '../../Utils/utils';
+import './newsCard.styles.css';
+
+import './newsCard.styles.css'
+const NewsCard = ({ title, description, content, url, urlToImage, source, publishedAt }) => {
+    return (
+        <div className="news-card-container" style={{ backgroundImage: `url(${urlToImage})` }}>
+            <div className="news-subCars-container">
+                <div className="news-title"><FontAwesomeIcon className="title-icon" icon={faNewspaper} />{title}</div>
+                <div className="news-publishedAt">{dateFormater(publishedAt)}</div>
+                <hr />
+                <div className="news-description">{description}</div>
+                <hr />
+                <div className="news-content">{content}</div>
+                <div className="card-footer row">
+                    <div className="news-source">{source}</div>
+                    <a className="news-link" href={url}>
+                        <FontAwesomeIcon className="" icon={faLink} />
+                    </a>
+                </div>
+            </div>
+        </div>
+    );
+}
+NewsCard.propTypes = {
+    title: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+    content: PropTypes.string,
+    url: PropTypes.string.isRequired,
+    urlToImage: PropTypes.string.isRequired,
+    source: PropTypes.string.isRequired,
+    publishedAt: PropTypes.string.isRequired,
+};
+export default NewsCard;
--- Client/src/index.js
+++ Client/src/index.js
@@ -4,6 +4,7 @@ import {BrowserRouter} from 'react-router-dom'
 import App from './App';
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/js/bootstrap";
+import 'font-awesome/css/font-awesome.min.css'
 import './index.css';
 
 ReactDOM.render(
