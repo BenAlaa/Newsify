843877e4e8d53ea669227f81f4eba8792252072f
BenAlaa <46384841+BenAlaa@users.noreply.github.com>
2020-04-04 06:13:17 +0200

    fix user service

--- Client/src/Services/user.service.js
+++ Client/src/Services/user.service.js
@@ -1,11 +1,18 @@
 import http from './http.service';
 import { apiUrl} from '../config.json';
 
-const apiEndpoint = apiUrl + "/auth";
+const apiEndpoint = apiUrl + "/users";
 function getUserUrl(id){
     return `${apiEndpoint}/${id}`;
 }
 
 export function getUser(userId){
     return http.get(getUserUrl(userId));
+}
+export function register(user){
+    return http.post(apiEndpoint,{
+        name:user.name,
+        email:user.email,
+        password:user.password
+    });
 }
--- Routes/auth.js
+++ Routes/auth.js
@@ -11,10 +11,10 @@ router.post("/", async (req, res) => {
   if (error) return res.status(400).send(error.details[0].message);
 
   let user = await User.findOne({ email: req.body.email });
-  if (!user) return res.status(400).send("Invalid Email or Password");
+  if (!user) return res.status(400).send({status:400, message: "Invalid Email or Password"});
 
   const validPassword = await bcrypt.compare(req.body.password, user.password);
-  if (!validPassword) return res.status(400).send("Invalid Email or Password");
+  if (!validPassword) return res.status(400).send({status:400, message: "Invalid Email or Password"});
 
   const token = user.generateAuthToken();
   res.status(200).send({status:200, data: token, message: "User Loggedin Succesfully"});
