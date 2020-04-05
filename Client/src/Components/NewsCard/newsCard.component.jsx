bdf58b941564505c53fcd2eb0e71b311b3286d9b
BenAlaa <46384841+BenAlaa@users.noreply.github.com>
2020-04-05 21:18:06 +0200

    edit news card style

--- Client/src/Components/NewsCard/newsCard.component.jsx
+++ Client/src/Components/NewsCard/newsCard.component.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from "prop-types";
 import { faLink, faNewspaper } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
