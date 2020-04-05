e8d14fb323c3cb0ff4d99fb5d5b59fb04a775ffc
BenAlaa <46384841+BenAlaa@users.noreply.github.com>
2020-04-05 21:17:41 +0200

    edit subscription service

--- Client/src/Services/source.service.js
+++ Client/src/Services/source.service.js
@@ -3,6 +3,10 @@ import { apiUrl} from '../config.json';
 
 const apiEndpoint = apiUrl + "/sources";
 
-export async function getSources(page=1, pageSize=20) {
+export async function getSources(page=1, pageSize=12) {
     return await http.get(apiEndpoint, {params: {page,pageSize}});
+}
+
+export async function editSubscription(source) {
+    return await http.put(`${apiUrl}/subscribe/switch`, {source})
 }
