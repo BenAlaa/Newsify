1aebaf9f8d27ff18d8ad287219ad25018e2610d6
BenAlaa <46384841+BenAlaa@users.noreply.github.com>
2020-06-07 18:24:53 +0200

    fix naming

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ if(process.env.NODE_ENV === 'production') {
   // Set static folder
   app.use(express.static('client/build'));
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    res.sendFile(path.resolve(__dirname, 'Client', 'build', 'index.html'))
   })
 }
 
